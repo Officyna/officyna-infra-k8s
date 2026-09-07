1e835297be9a429eac0b2b3df20805069873106b
anaeliza12 <anaelizaperobelli@gmail.com>
2026-09-07 19:35:32 -0300

    feat: instrumentando k8s no new relic

--- data.tf
+++ data.tf
@@ -5,3 +5,10 @@ data "aws_iam_user" "principal_user" {
 data "aws_iam_user" "github_actions_user" {
   user_name = "github-actions-officyna"
 }
+data "aws_eks_cluster" "cluster" {
+  name = aws_eks_cluster.cluster.name
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = aws_eks_cluster.cluster.name
+}
